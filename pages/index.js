e4824e120d5fb211afe16d281f72cbcefec1f4a3
eang0521 <57340860+eang0521@users.noreply.github.com>
2020-07-15 21:10:20 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,28 +8,28 @@ import Date from '../components/date'
 export default function Home({ allPostsData}) {
     return (
 	    <Layout home>
-	    <Head>
-	    <title>Pink Ogre Games Blog</title>
-	    </Head>
-	    <section className={utilStyles.headingMd}>
-	    <p>Welcome to the <b>official</b> Pink Ogre Games Blog! We'll write about all kinds of things here, from our favorite board games, to our least favorite game mechanics! We'll post new content as often as possible (hopefully everyday!)</p>
-	    </section>
-	    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-	    <h2 className={utilStyles.headingLg}>Blog</h2>
-	    <ul className={utilStyles.list}>
-	    {allPostsData.map(({ id, date, title }) => (
-		    <li className={utilStyles.listItem} key={id}>
-		    <Link href="/posts/[id]" as={`/posts/${id}`}>
-		    <a>{title}</a>
-		    </Link>
-		    <br />
-		    <small className={utilStyles.lightText}>
-		    <Date dateString={date} />
-		    </small>
-		    </li>
-	    ))}
-	</ul>
-	    </section>
+	      <Head>
+	        <title>Pink Ogre Games Blog</title>
+	      </Head>
+	      <section className={utilStyles.headingMd}>
+	        <p>Welcome to the <b>official</b> Pink Ogre Games Blog! We'll write about all kinds of things here, from our favorite board games, to our least favorite game mechanics! We'll post new content as often as possible (hopefully everyday!)</p>
+	      </section>
+	      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+	        <h2 className={utilStyles.headingLg}>Blog</h2>
+	          <ul className={utilStyles.list}>
+	            {allPostsData.map(({ id, date, title }) => (
+		            <li className={utilStyles.listItem} key={id}>
+		              <Link href="/posts/[id]" as={`/posts/${id}`}>
+		                <a>{title}</a>
+		              </Link>
+		              <br />
+		              <small className={utilStyles.lightText}>
+		                <Date dateString={date} />
+		              </small>
+		            </li>
+	            ))}
+	          </ul>
+	      </section>
 	    </Layout>
     )
 }
