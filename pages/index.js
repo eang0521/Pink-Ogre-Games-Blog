626f38a36a547638ee7504d2c296f305f696c1a9
Elijah Ang <eang0521@users.noreply.github.com>
2020-08-06 04:45:09 +0000

    another index.js update :)

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,11 @@ import Link from 'next/link'
 import Date from '../components/date'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Carousel from 'react-bootstrap/Carousel'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function Home({ allPostsData }) {
   return (
@@ -13,7 +18,55 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>Pink Ogre Games Blog</title>
       </Head>
-          <Carousel>
+          <Jumbotron fluid style={{padding: 25}}>
+          <h1 style={{textAlign: "center", fontSize: 50}}>Pink Ogre Games</h1>
+          <p style={{textAlign: "center"}}>Welcome to the <b>official</b> Pink Ogre Games Blog! We'll write about all kinds of things he\
+re, from our favorite board games, to our least favorite game mechanics! We'll post new content as often as possible (hopefully everyday!)</p>
+                </Jumbotron>
+<br />
+          <Container>
+          <Row>
+          <Col>
+          <Card>
+          <Card.Img variant="top" src="/images/Hoops.jpg" />
+          <Card.Body>
+          <Card.Title>Hoops!</Card.Title>
+          <Card.Text>
+          Play a 1-on-1 battle of basketball!
+      </Card.Text>
+          <Button variant="primary" href="https://www.thegamecrafter.com/games/hoops-" target="_blank">Buy Hoops!</Button>
+          </Card.Body>
+          </Card>
+          </Col>
+          <Col style={{ width: '18rem' }}>
+          <Card>
+          <Card.Img variant="top" src="/images/StAnSmash.jpg" />
+          <Card.Body>
+          <Card.Title>Stuffed Animal Smash</Card.Title>
+          <Card.Text>Battle with your favorite Stuffed Animals!</Card.Text>
+<Button variant="primary" href="https://www.thegamecrafter.com/games/stuffed-animal-smash" target="_blank">View StAn Smash!</Button>
+      </Card.Body>
+          </Card>
+          </Col>
+          </Row>
+          </Container>
+          <br />
+          <Container>
+          <Row>
+                    <Col style={{ width: '18rem' }}>
+          <Card>
+          <Card.Img variant="top" src="/images/SpaceTrader.jpg" />
+          <Card.Body>
+          <Card.Title>Space Trader</Card.Title>
+          <Card.Text>Earn your living as a space merchant!</Card.Text>
+          <Button variant="primary" href="https://www.thegamecrafter.com/games/space-trader5" target="_blank">Buy Space Trader!</Button>
+          </Card.Body>
+          </Card>
+          </Col>
+          </Row>
+          </Container>
+          <br />
+          {/*          <Carousel>
           <Carousel.Item>
           <a href="https://www.thegamecrafter.com/games/hoops-"><img
       className="d-block w-100"
@@ -50,11 +103,7 @@ export default function Home({ allPostsData }) {
           </Carousel.Caption>
           </Carousel.Item>
           </Carousel>
-          <br />
-          <Jumbotron fluid style={{padding: 25}}>
-          <h1 style={{textAlign: "center", fontSize: 50}}>Pink Ogre Games</h1>
-        <p style={{textAlign: "center"}}>Welcome to the <b>official</b> Pink Ogre Games Blog! We'll write about all kinds of things here, from our favorite board games, to our least favorite game mechanics! We'll post new content as often as possible (hopefully everyday!)</p>
-      </Jumbotron>
+          <br />*/}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
